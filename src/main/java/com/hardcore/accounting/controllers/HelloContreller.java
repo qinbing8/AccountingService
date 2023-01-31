74a71496abc066979770b565c544a0a98d1f2fb4
BingQin <qinbing08@gmail.com>
2023-01-31 21:56:14 +0800

    feat(controllers): Implement the hello controller for first API

---
+++ src/main/java/com/hardcore/accounting/controllers/HelloContreller.java
@@ -0,0 +1,20 @@
+package com.hardcore.accounting.controllers;
+
+import com.hardcore.accounting.model.Greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloContreller {
+
+    private AtomicLong counter = new AtomicLong();
+
+    @GetMapping(path = "v1/greeting")
+    public Greeting setHello(@RequestParam("name") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format("Hollo, %s", name));
+    }
+}
