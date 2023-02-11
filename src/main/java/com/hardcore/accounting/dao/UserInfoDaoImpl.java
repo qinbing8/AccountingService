3ddde2e41450f49706329804c9d7e23e8edfd4cd
BingQin <qinbing08@gmail.com>
2023-02-11 22:31:39 +0800

    feat(controller): 实现getUserInfoByUserId API 1. 集成Mybatis 2. 添加相应配置 3. 实现getUserInfoByUserId相关逻辑

--- src/main/java/com/hardcore/accounting/dao/UserInfoDaoImpl.java
+++ src/main/java/com/hardcore/accounting/dao/UserInfoDaoImpl.java
@@ -1,14 +1,24 @@
 package com.hardcore.accounting.dao;
 
+import com.hardcore.accounting.dao.mapper.UserInfoMapper;
 import com.hardcore.accounting.model.persistence.UserInfo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserInfoDaoImpl implements UserInfoDao {
+
+    private UserInfoMapper userInfoMapper;
+
+    @Autowired
+    public UserInfoDaoImpl(UserInfoMapper userInfoMapper) {
+        this.userInfoMapper = userInfoMapper;
+    }
+
     @Override
     public UserInfo getUserInfoById(Long id) {
-        return null;
+        return userInfoMapper.getUserInfoById(id);
     }
 
     @Override
